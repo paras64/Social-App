c055e95f2b325a66225d71ff432644d2d0ba6712
paras64 <146513526+paras64@users.noreply.github.com>
2024-10-28 22:38:18 +0530

    pp

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -64,7 +64,7 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      "https://social-app-qbv7-lubmwvd9j-paras64s-projects.vercel.app/",
+      "http://localhost:3001/",
       {
         method: "POST",
         body: formData,
@@ -79,7 +79,7 @@ const Form = () => {
   };
 
   const login = async (values, onSubmitProps) => {
-    const loggedInResponse = await fetch("https://social-app-qbv7-lubmwvd9j-paras64s-projects.vercel.app/", {
+    const loggedInResponse = await fetch("http://localhost:3001/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
