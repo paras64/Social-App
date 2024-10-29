f553595845a4d7ef403bf281a584e20f1fba7d7d
paras64 <146513526+paras64@users.noreply.github.com>
2024-10-29 09:33:17 +0530

    Update Form.jsx

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -64,7 +64,7 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      "http://localhost:3001/",
+      "http://localhost:3001",
       {
         method: "POST",
         body: formData,
@@ -79,7 +79,7 @@ const Form = () => {
   };
 
   const login = async (values, onSubmitProps) => {
-    const loggedInResponse = await fetch("http://localhost:3001/", {
+    const loggedInResponse = await fetch("http://localhost:3001", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
